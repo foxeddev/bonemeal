d10090269dc5ca91088fd2a67b5dcbf27454ab55
FoxedDev <foxed.dev@gmail.com>
2026-05-23 14:13:22 +0200

    Add resource pack project type.

---
+++ src/packseed/core/generate/resource_pack.py
@@ -0,0 +1,30 @@
+"""Function to generate a new resource pack."""
+
+import json
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from packseed.core.mc_version import MCVersion
+
+
+def generate_resource_pack(
+    path: Path,
+    description: str | None,
+    mc_version: MCVersion,
+) -> None:
+    """Generate a new resource pack."""
+    with Path.open(path / "pack.mcmeta", "x") as f:
+        json.dump(
+            {
+                "pack": {
+                    "description": description,
+                    "min_format": [mc_version.resource_pack_version],
+                    "max_format": [mc_version.resource_pack_version],
+                },
+            },
+            fp=f,
+            indent=2,
+        )
+
+    Path.mkdir(path / "assets" / path.name, parents=True)
--- src/packseed/core/project_types/main.py
+++ src/packseed/core/project_types/main.py
@@ -1,6 +1,7 @@
 """Register all project types."""
 
 from packseed.cli.create.data_pack import create_data_pack
+from packseed.cli.create.resource_pack import create_resource_pack
 from packseed.core.project_types.project_type import ProjectType
 
 PROJECT_TYPES: dict[str, ProjectType] = {
@@ -10,5 +11,12 @@ PROJECT_TYPES: dict[str, ProjectType] = {
         create=create_data_pack,
         aliases=["datapack", "dp"],
         title="Data pack",
+    ),
+    "resource_pack": ProjectType(
+        id="resource_pack",
+        name="resource-pack",
+        create=create_resource_pack,
+        aliases=["resourcepack", "rp"],
+        title="Resource pack",
     ),
 }
