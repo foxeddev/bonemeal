28eabd3c7cb48ad531e2f23e3f1b1372bed59006
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-06-11 20:36:50 +0200

    Add custom styling to the command help screen

--- src/bonemeal/cli/main.py
+++ src/bonemeal/cli/main.py
@@ -6,10 +6,11 @@ from prompt_toolkit.shortcuts import set_title
 from bonemeal.cli.create.main import create_project
 from bonemeal.cli.errors import handle_errors
 from bonemeal.cli.list.main import list_project_types
-from bonemeal.cli.utils import add_command
+from bonemeal.cli.utils import add_command, help_config
 
 
 @rich_click.group(context_settings={"help_option_names": ("-h", "--help")})
+@rich_click.rich_config(help_config=help_config)
 @handle_errors
 def main() -> None:
     """Create and manage Minecraft packs."""
