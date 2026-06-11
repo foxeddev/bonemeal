41eb64cbff87ea8790080ad9c44de18910e6ac40
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-06-11 20:39:48 +0200

    Use helper function for command execution in GIT author finding

--- src/bonemeal/core/author.py
+++ src/bonemeal/core/author.py
@@ -2,15 +2,13 @@
 
 import subprocess
 
+from bonemeal.core.utils import check_silent
+
 
 def get_git_username() -> str | None:
     """Try to find the user's Git username or return None."""
     try:
-        return subprocess.check_output(
-            # this is fine because no sensitive data is passed
-            ["git", "config", "user.name"],  # noqa: S607
-            text=True,
-        ).strip()
+        return check_silent(["git", "config", "user.name"]).strip()
 
-    except subprocess.CalledProcessError:
+    except subprocess.CalledProcessError, subprocess.TimeoutExpired:
         return None
