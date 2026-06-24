ffc21f927dcc08d85e55a6d78131801a2d5396e8
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-06-24 15:28:36 +0200

    Use info message type for welcome messages

--- src/bonemeal/cli/commons/messages.py
+++ src/bonemeal/cli/commons/messages.py
@@ -1,12 +1,12 @@
 """Common messages used by multiple commands."""
 
-from bonemeal.cli.components.message import success_message
+from bonemeal.cli.components.message import info_message
 from bonemeal.cli.components.utils import LineMode
 
 
 def welcome_message() -> None:
     """Send a welcome message."""
-    success_message(
+    info_message(
         title="Welcome to Bone Meal!",
         description="The Minecraft pack management CLI.",
         icon="🦴",
