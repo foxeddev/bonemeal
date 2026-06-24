30aa48b8c03aeea00258e18d4f47e71aae8320b4
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-06-24 15:32:15 +0200

    Improve error handling when fetching author name with Git

--- src/bonemeal/core/fields/author.py
+++ src/bonemeal/core/fields/author.py
@@ -2,13 +2,21 @@
 
 import subprocess
 
+from bonemeal.core.errors.main import BoneMealError
 from bonemeal.core.utils.commands import check
+
+
+class GitExecutionError(BoneMealError):
+    """Error raised when the `git`-command raises an error."""
+
+    title = "Git raised an error!"
 
 
 def get_git_username() -> str | None:
     """Try to find the user's Git username or return None."""
     try:
         return check(["git", "config", "user.name"]).strip()
-
-    except subprocess.CalledProcessError, subprocess.TimeoutExpired:
+    except FileNotFoundError:
         return None
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as err:
+        raise GitExecutionError from err
