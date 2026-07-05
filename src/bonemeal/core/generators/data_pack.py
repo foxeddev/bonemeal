2446c4490dc3e3a624c2a2f1c581b1829e8f655d
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2026-07-05 21:11:27 +0200

    Implement tick and load function generation for data packs

--- src/bonemeal/core/generators/data_pack.py
+++ src/bonemeal/core/generators/data_pack.py
@@ -16,17 +16,64 @@ def generate_data_pack(
     template: Template,
 ) -> None:
     """Generate a new data pack."""
-    with Path.open(path / "pack.mcmeta", "x") as f:
-        json.dump(
-            {
-                "pack": {
-                    "description": description,
-                    "min_format": mc_version.data_pack_version,
-                    "max_format": mc_version.data_pack_version,
-                },
-            },
-            fp=f,
-            indent=2,
-        )
+    namespace = path.name
 
-    Path.mkdir(path / "data" / path.name, parents=True)
+    if "pack_mcmeta" in template.includes:
+        with Path.open(path / "pack.mcmeta", "x") as f:
+            json.dump(
+                {
+                    "pack": {
+                        "description": description,
+                        "min_format": mc_version.data_pack_version,
+                        "max_format": mc_version.data_pack_version,
+                    },
+                },
+                fp=f,
+                indent=2,
+            )
+
+    if "namespace" in template.includes:
+        Path.mkdir(path / "data" / namespace, parents=True, exist_ok=True)
+
+        if "load_tick" in template.includes:
+            load_tag_path = (
+                path / "data" / "minecraft" / "tags" / "function" / "load.json"
+            )
+            Path.mkdir(load_tag_path.parent, parents=True, exist_ok=True)
+            with Path.open(load_tag_path, "x") as f:
+                json.dump(
+                    {
+                        "values": [
+                            f"{namespace}:load",
+                        ],
+                    },
+                    f,
+                    indent=2,
+                )
+
+            tick_tag_path = (
+                path / "data" / "minecraft" / "tags" / "function" / "tick.json"
+            )
+            Path.mkdir(tick_tag_path.parent, parents=True, exist_ok=True)
+            with Path.open(tick_tag_path, "x") as f:
+                json.dump(
+                    {
+                        "values": [
+                            f"{namespace}:tick",
+                        ],
+                    },
+                    f,
+                    indent=2,
+                )
+
+            load_function_path = (
+                path / "data" / namespace / "function" / "load.mcfunction"
+            )
+            Path.mkdir(load_function_path.parent, parents=True, exist_ok=True)
+            Path.touch(load_function_path)
+
+            tick_function_path = (
+                path / "data" / namespace / "function" / "tick.mcfunction"
+            )
+            Path.mkdir(tick_function_path.parent, parents=True, exist_ok=True)
+            Path.touch(tick_function_path)
